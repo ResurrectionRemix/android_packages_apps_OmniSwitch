a86899e1b3543e4294274bf11ac5af837df11e5e
Max Weninger <max.weninger@gmail.com>
2014-02-13 01:06:06 +0100

    OmniSwitch: fixed drag handle placing in immersive mode
    Need to use different window type to get correct placement
    of the drag handle depending if the navigation bar is shown
    or not
    
    Change-Id: I2c02ef4bdaba19a3fbd5be59b4182602fade886f

--- src/org/omnirom/omniswitch/ui/SwitchGestureView.java
+++ src/org/omnirom/omniswitch/ui/SwitchGestureView.java
@@ -211,7 +211,7 @@ public class SwitchGestureView implements OnShowcaseEventListener {
         WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.TYPE_PHONE,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                         | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                         | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,

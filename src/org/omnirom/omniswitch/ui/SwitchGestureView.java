7fb7da75933f8390b738e8a4f5eb0eea9485ae84
maxwen <max.weninger@gmail.com>
2014-10-05 16:54:40 +0200

    OmniSwitch: fix rotation of drag handle after position change
    Change-Id: I5ebb44ce7c8e9f3187caa10dabda0975431b18bd

--- src/org/omnirom/omniswitch/ui/SwitchGestureView.java
+++ src/org/omnirom/omniswitch/ui/SwitchGestureView.java
@@ -392,7 +392,7 @@ public class SwitchGestureView implements OnShowcaseEventListener {
             }
         });
         mView.addView(mDragButton, getDragHandleLayoutParamsSmall());
-        updateButton();
+        updateButton(false);
     }
 
     private int getGravity() {
@@ -458,10 +458,14 @@ public class SwitchGestureView implements OnShowcaseEventListener {
         return lp;
     }
 
-    private void updateButton() {
+    private void updateButton(boolean reload) {
         if(mConfiguration.mAutoHide){
             updateDragHandleImage(false);
         } else {
+            if (reload) {
+                // to catch location/rotation changes
+                updateDragHandleImage(false);
+            }
             updateDragHandleImage(true);
         }
     }
@@ -500,7 +504,7 @@ public class SwitchGestureView implements OnShowcaseEventListener {
         if(DEBUG){
             Log.d(TAG, "updatePrefs");
         }
-        updateButton();
+        updateButton(true);
 
         mSpeedSwitcher = prefs.getBoolean(SettingsActivity.PREF_SPEED_SWITCHER, false);
         String favoriteListString = prefs.getString(SettingsActivity.PREF_FAVORITE_APPS, "");

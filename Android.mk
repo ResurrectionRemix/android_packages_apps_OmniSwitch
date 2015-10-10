bb725621a2c8e11b23f24984d64d362e09ce783b
Varun <date.varun123@gmail.com>
2015-10-10 13:12:18 +0530

    OmniSwitch:Dark Material Theme (PB)
    Change-Id: I82f4934091baca01c805aba3d63947ed13df79c6

--- Android.mk
+++ Android.mk
@@ -16,6 +16,8 @@
 
 LOCAL_PATH := $(call my-dir)
 
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := OmniSwitch
